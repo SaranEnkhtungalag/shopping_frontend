a4b916accd7b4cc835d8bd9bc685b21ff60f2f03
SaranEnkhtungalag <tungaa.love61@gmail.com>
2023-11-27 12:28:04 +0100

    Verbindung von Backend und Frontend

---
+++ src/views/ShoppingListView.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import axios from "axios";
+export default {
+  data() {
+    return {
+      shoppingLists: {}
+    };
+  },
+  mounted() {
+    axios
+      .get('http://localhost:8080/shoppingLists')
+      .then((response) => {
+        this.shoppingLists = response.data;
+      });
+  }
+};
+</script>
+
+<template>
+  {{shoppingLists}}
+</template>
+
+
+<style scoped>
+
+</style>
